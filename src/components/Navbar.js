91778217cc3e060db521fafd8a06c96122f2a176
edu <edwinnjoroge001@gmail.com>
2024-05-09 12:38:01 +0300

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <a href="#">Home</a>
+                <a href="#">contact</a>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
